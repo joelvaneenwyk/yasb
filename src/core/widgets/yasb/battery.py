d1f6a157a61ef23e3d12c87bf182e736a0dc7c7f
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-06-14 18:27:03 -0700

    Fix trivial errors
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- src/core/widgets/yasb/battery.py
+++ src/core/widgets/yasb/battery.py
@@ -74,7 +74,7 @@ class BatteryWidget(BaseWidget):
 
         if secs_left == psutil.POWER_TIME_UNLIMITED:
             time_left = "unlimited"
-        elif type(secs_left) == int:
+        elif isinstance(secs_left, int):
             time_left = timedelta(seconds=secs_left)
             time_left = humanize.naturaldelta(time_left) if self._time_remaining_natural else str(time_left)
         else:
